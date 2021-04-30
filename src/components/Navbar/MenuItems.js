ef471576a964ff5a744b6f6b80161d74d904e864
Rodrigo <61331491+rodrigobergamindev@users.noreply.github.com>
2021-04-30 01:34:01 -0300

    criando navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,33 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sobre',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Produtos',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contato',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Pagamento',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Como Comprar',
+        url: '#',
+        cName: 'nav-links'
+    }
+
+]
