589eddbfc759b03ed2dac911d498a47b07cee666
Rodrigo <rodrigobergamindev@gmail.com>
2021-05-12 15:57:00 -0300

    atualizando icones social media

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -90,10 +90,10 @@ const Navbar = () => {
       </div>
       <Burger />
     <SocialMediaList>
-      <li><a href="https://vm.tiktok.com/ZMegKPN55/" target="_blank"><img src="/img/instagram.svg"/></a></li>
-      <li><a href="https://www.instagram.com/lojinha_da_mayumi/" target="_blank"><img src="/img/tiktok.svg"/></a></li>
-      <li><a href="https://www.facebook.com/lojinhadamayumi/" target="_blank"><img src="/img/facebook.svg"/></a></li>
-      <li><a href="https://api.whatsapp.com/send?phone=551129639226&text=Ol%C3%A1%2C%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20as%20pe%C3%A7as%20da%20loja" target="_blank"><img src="/img/whatsapp.svg"/></a></li>
+      <li><a href="https://vm.tiktok.com/ZMegKPN55/" target="_blank"><img src="/img/instagram.png"/></a></li>
+      <li><a href="https://www.instagram.com/lojinha_da_mayumi/" target="_blank"><img src="/img/tiktok.png"/></a></li>
+      <li><a href="https://www.facebook.com/lojinhadamayumi/" target="_blank"><img src="/img/facebook.png"/></a></li>
+      <li><a href="https://api.whatsapp.com/send?phone=551129639226&text=Ol%C3%A1%2C%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20as%20pe%C3%A7as%20da%20loja" target="_blank"><img src="/img/whatsapp.png"/></a></li>
     </SocialMediaList>
     </Nav>
   )
