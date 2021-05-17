f83486da6f16e6087aa6845716fc571998cafe66
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-05-17 16:54:31 -0300

    navbar

--- src/components/Navbar/RightNav.tsx
+++ src/components/Navbar/RightNav.tsx
@@ -3,68 +3,7 @@ import {MenuItems} from './MenuItems'
 import React from 'react';
 import styled from 'styled-components';
 
-const Ul = styled.ul`
-    list-style: none;
-    display: flex;
-    flex-flow: row nowrap;
-    align-items: center;
-    flex: 5;
- 
-
-
-  @media(min-width: 931px){
-    justify-content: center;
-    
-   
-    li {
-      color: var(--secondary);
-      padding: 0.7rem;
-    }
-  
-      a{ 
-          padding: 10px;
-          font-size: 0.9em;
-          letter-spacing: 0.21rem;
-          transition: 0.5s linear;
-
-          &:hover {
-            color: var(--terciary);
-        }
-  }
-
-
-
-
-  }
-  @media (max-width: 931px) {
-
-    flex-flow: column nowrap;
-    background-color: white;
-    color: #FC5C65;
-    position: fixed;
-    transform: ${({ open }) => open ? 'translateY(0)' : 'translateY(-150%)'};
-    top: ${({ open }) => open ? '55px' : '10px'};
-    right: 0;
-    width: 100%;
-    height: 100vh;
-    transition: transform 0.3s ease-in-out;
-
-
-
-    li {
-      padding: 0.7rem;
-      width: 100%;
-      text-align: center;
-      
-      a {
-        font-size: 2em;
-
-      }
-
-    }
-  }
-`;
-
+import {Ul} from './styles'
 
 
 const RightNav = ({ open }) => {
